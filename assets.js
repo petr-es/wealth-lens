07b1a30754a57a67a32a5bcecd060994cdfa0b3a
Petr Slavik <pslavik1@gmail.com>
2026-05-18 16:34:17 +0200

    asset upd

--- assets.js
+++ assets.js
@@ -19,7 +19,7 @@ const ASSETS = {
     name:     'SentinelOne',
     yahooUrl: 'https://finance.yahoo.com/quote/S',
     currency: 'USD',
-    holdings: { ibkr: 1257, etrade: 122 },
+    holdings: { ibkr: 1000, etrade: 122 },
   },
   alpha: {
     ticker:   'ALPHA',
